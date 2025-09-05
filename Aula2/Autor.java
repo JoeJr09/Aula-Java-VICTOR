3290a6fc1c3e006c89a557c94158edf3407cc368
JoeJr09 <164202659+JoeJr09@users.noreply.github.com>
2025-09-05 12:16:09 -0300

    att aula2

--- Aula2/Autor.java
+++ Aula2/Autor.java
@@ -1,15 +1,37 @@
 package Aula2;
 public class Autor {
-    String nomeAutor;
-    String emailAutor;
-    String cpfAutor;
+    private String nomeAutor;
+    private String emailAutor;
+    private String CPFAutor;
 
     void mostrarDetalhesAutor(){
-        String mensagem = "Detalhes do autor";
-        System.out.println(mensagem);
+        System.out.println("Detalhes do autor");
         System.out.println("Nome: " + nomeAutor);
         System.out.println("Email: " + emailAutor);
-        System.out.println("CPF: " + cpfAutor);
+        System.out.println("CPF: " + CPFAutor);
+    }
 
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public String getEmailAutor() {
+        return emailAutor;
+    }
+
+    public void setEmailAutor(String emailAutor) {
+        this.emailAutor = emailAutor;
+    }
+
+    public String getCPFAutor() {
+        return CPFAutor;
+    }
+
+    public void setCPFAutor(String CPFAutor) {
+        this.CPFAutor = CPFAutor;
     }
 }
